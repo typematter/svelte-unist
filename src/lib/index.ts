99a2d95a5817c180d2b7354f86101fa9bd8d2bea
Matthew Gibbons <mhgibbons@me.com>
2024-10-04 21:02:11 +0000

    Exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,4 @@
-export { default as Hast } from './components/Unist.svelte';
+export { default as Node } from './components/Node.svelte';
+export { default as Unist } from './components/Unist.svelte';
+export { default as getUnistContext } from './contexts/get-unist-context.js';
+export { default as setUnistContext } from './contexts/set-unist-context.js';
