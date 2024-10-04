5e7b50ee1df22ee005164f4f69db46384891d033
Matthew Gibbons <mhgibbons@me.com>
2024-10-04 21:10:12 +0000

    Export `Components`

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,4 @@ export { default as Node } from './components/Node.svelte';
 export { default as Unist } from './components/Unist.svelte';
 export { default as getUnistContext } from './contexts/get-unist-context.js';
 export { default as setUnistContext } from './contexts/set-unist-context.js';
+export type { Components } from './types/components.js';
