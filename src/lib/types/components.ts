22c1d876ea2b7d0f18b25a3e6a36b8988e85c44f
Matthew Gibbons <mhgibbons@me.com>
2024-10-05 16:32:45 +0000

    Extend from `Nodes` by default

--- src/lib/types/components.ts
+++ src/lib/types/components.ts
@@ -1,6 +1,7 @@
 import type { Component } from 'svelte';
 import type { Node } from 'unist';
+import type { Nodes } from './nodes.js';
 
-export type Components<T extends Node> = {
+export type Components<T extends Node = Nodes> = {
 	[K in T['type']]: Component<Extract<T, { type: K }>> | undefined;
 };
