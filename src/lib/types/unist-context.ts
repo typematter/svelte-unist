3132dd43e76176d43a2057467f64ab5ed9c3f76b
Matthew Gibbons <mhgibbons@me.com>
2024-10-05 16:33:20 +0000

    Use default

--- src/lib/types/unist-context.ts
+++ src/lib/types/unist-context.ts
@@ -1,6 +1,5 @@
-import type { Node } from 'unist';
 import type { Components } from './components.js';
 
 export interface UnistContext {
-	components: Components<Node>;
+	components: Components;
 }
