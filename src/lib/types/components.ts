137c667e7e30e44329b0149c4dc36fcd2d008197
Matthew Gibbons <mhgibbons@me.com>
2024-10-05 04:58:24 +0000

    Allow component to be undefined

--- src/lib/types/components.ts
+++ src/lib/types/components.ts
@@ -2,5 +2,5 @@ import type { Component } from 'svelte';
 import type { Node } from 'unist';
 
 export type Components<T extends Node = Node> = {
-	[K in T['type']]: Component<Extract<T, { type: K }>>;
+	[K in T['type']]: Component<Extract<T, { type: K }>> | undefined;
 };
