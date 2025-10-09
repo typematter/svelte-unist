507bfe51bdde92d0de31070f88115dc4b56d3091
Matthew Gibbons <matthew@southwestkoala.co.uk>
2025-10-09 11:07:36 +0100

    Export UnistContext

--- src/lib/components/unist/index.ts
+++ src/lib/components/unist/index.ts
@@ -1,5 +1,5 @@
 import { getUnistContext } from './get-unist-context.js';
 import Node from './node.svelte';
-import Root, { type ComponentMap, type Components } from './unist.svelte';
+import Root, { type ComponentMap, type Components, type UnistContext } from './unist.svelte';
 
-export { getUnistContext, Node, Root, type ComponentMap, type Components };
+export { getUnistContext, Node, Root, type ComponentMap, type Components, type UnistContext };
