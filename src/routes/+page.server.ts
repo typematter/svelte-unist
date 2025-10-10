a97c20b6a78111cfdd1ac351f569c9eee26fd383
Matthew Gibbons <mhgibbons@me.com>
2025-10-10 05:04:43 +0000

    Remove unused page server load implementation

--- src/routes/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { Node } from 'unist';
-import { u } from 'unist-builder';
-import type { PageServerLoad } from './$types.js';
-
-export const load = (async () => {
-	const ast: Node = u('root', [u('text', 'Hello, World!')]);
-
-	return { ast };
-}) satisfies PageServerLoad;
