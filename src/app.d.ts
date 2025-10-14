08252418a053994c8a50a2cac0d53f364236ad92
Matthew Gibbons <mhgibbons@me.com>
2025-10-14 04:39:22 +0000

    Removed interface

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,6 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	// Global function to render math in an element
-	function renderMathInElement(element: HTMLElement): void;
-
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
@@ -13,4 +10,5 @@ declare global {
 	}
 }
 
-export {};
+export { };
+
