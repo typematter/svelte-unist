5080df6a91a6fc33fbe7b84c227af0da83b8b0d9
Matthew Gibbons <mhgibbons@me.com>
2025-10-14 06:11:03 +0000

    Build

--- dist/components/unist/get-unist-context.js
+++ dist/components/unist/get-unist-context.js
@@ -1,3 +1,3 @@
 import { getContext } from 'svelte';
 import { CONTEXT_SYMBOL } from './context.symbol.js';
-export const getUnistContext = () => getContext(CONTEXT_SYMBOL);
+export const getUnistContext = () => getContext(CONTEXT_SYMBOL) || {};
