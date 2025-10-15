08fcf6c12ec0a6026097bd0d04fcba84d953aa35
Matthew Gibbons <mhgibbons@me.com>
2025-10-15 06:53:23 +0000

    Refactor getUnistContext to include context check and error handling

--- src/lib/components/unist/get-unist-context.ts
+++ src/lib/components/unist/get-unist-context.ts
@@ -1,5 +1,11 @@
-import { getContext } from 'svelte';
+import { getContext, hasContext } from 'svelte';
 import { CONTEXT_SYMBOL } from './context.symbol.js';
 import type { UnistContext } from './unist-context.js';
 
-export const getUnistContext = () => getContext<UnistContext>(CONTEXT_SYMBOL) || {} as UnistContext;
+export const getUnistContext = () => {
+    if (hasContext(CONTEXT_SYMBOL)) {
+        return getContext<UnistContext>(CONTEXT_SYMBOL);
+    }
+
+    throw new Error('getUnistContext() must be used within a <Unist> component');
+}
