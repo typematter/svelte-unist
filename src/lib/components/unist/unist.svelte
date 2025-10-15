873f35e79eee9069face49a88d47625b77cee540
Matthew Gibbons <mhgibbons@me.com>
2025-10-15 11:16:24 +0000

    Update imports

--- src/lib/components/unist/unist.svelte
+++ src/lib/components/unist/unist.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import Node from './node.svelte';
-	import { setUnistContext } from './set-unist-context.js';
-	import type { UnistContext } from './unist-context.js';
+	import { setUnistContext, type UnistContext } from './unist-context.js';
 
 	let context: UnistContext = $props();
 
