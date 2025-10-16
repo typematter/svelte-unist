191e8d4584fd82c1028ef483ee654223368716f9
Matthew Gibbons <mhgibbons@me.com>
2025-10-16 05:30:20 +0000

    Format

--- src/lib/components/unist/index.ts
+++ src/lib/components/unist/index.ts
@@ -3,4 +3,3 @@ export * from './components.js';
 export { default as Node } from './node.svelte';
 export { getUnistContext, type UnistContext } from './unist-context.js';
 export { default as Unist } from './unist.svelte';
-
